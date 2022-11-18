391ac0b27495b050574e4b4d194ad5b32746a8db
Luis Rivera <luisantonioriveraalvarado@gmail.com>
2022-11-17 18:11:39 -0600

    Initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NotpagefoundComponent } from './pages/notpagefound/notpagefound.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotpagefoundComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
